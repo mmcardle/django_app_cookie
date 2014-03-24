0f3aa46ec26718999f9c5454a2f7ceeccc914cbd
mmcardle <mark.mcardle@sohonet.com>
2014-03-24 21:46:20 +0000

    Generate pre hook now generates with jinja2, removed generated templates, updated base templates with context

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -3,13 +3,11 @@ __author__ = 'mm'
 
 import json
 import os
-import shutil
 
 print "Start Generate Templates Hook"
 
 cookie_dir = '{{cookiecutter.app_name}}'
 project_base = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-
 json_file = os.path.join(project_base, 'cookiecutter.json')
 base_templates = os.path.join(project_base , 'base_templates')
 cookie_templates = os.path.join(project_base, cookie_dir,
@@ -17,13 +15,32 @@ cookie_templates = os.path.join(project_base, cookie_dir,
 
 js = json.load(open(json_file, 'r'))
 
+from jinja2 import FileSystemLoader
+from jinja2.environment import Environment
+from jinja2 import Template
+
+env = Environment()
+env.loader = FileSystemLoader(".")
+
 for model in js['models']:
     for f in os.listdir(base_templates):
         basename = os.path.basename(f)
         cookie_filename = "%s_%s" % (model.lower(), basename)
         base_file = os.path.join(base_templates, f)
         cookie_file = os.path.join(cookie_templates, cookie_filename)
-        shutil.copy(base_file, cookie_file)
+
+        ctx = js.copy()
+        ctx.update({'model': model})
+
+        _f_in = open(base_file, 'r')
+        template = Template(_f_in.read())
+        _f_in.close()
+
+        output = template.render(**ctx)
+
+        _f_out = open(cookie_file, 'wb')
+        _f_out.write('{%% raw %%}%s{%% endraw %%}' % output)
+        _f_out.close()
         print "Created Template %s" % cookie_file
 
 print "End Generate Templates Hook"
